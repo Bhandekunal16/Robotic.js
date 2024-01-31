a1fe2127cc4ed9ed6d384de67d92f52c5b4c38be
bhandekunal16 <bhandekunal16@gmail.com>
2024-01-31 15:56:11 +0530

    created

---
+++ main.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const Color = require("./interface/color");
+const Logger = require("./interface/Logger");
+const Node = require("./module/index");
+
+const app = express();
+app.use(bodyParser.json());
+const port = 3000;
+
+const color = new Color();
+const logger = new Logger();
+const node = new Node();
+
+app.get("/", async (req, res) => {
+  const query = "hello world";
+  res.send(query);
+});
+
+const routes = [Node, Color, Logger];
+let imports = routes.map((elements) => elements.name);
+
+app.listen(port, async () => {
+  logger.log("***************");
+  logger.new(imports);
+  logger.log(`Node app is successfully created on http://localhost: ${port}.`);
+  logger.log("***************");
+  node.getUserInput();
+});
