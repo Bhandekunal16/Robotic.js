13d345c961975f100698fe1e757574a0e67e0cd9
bhandekunal16 <bhandekunal16@gmail.com>
2024-02-02 17:31:15 +0530

    Added a new function to generate random emails.

---
+++ class/uuid.js
@@ -0,0 +1,59 @@
+/** class contains the uuid generating  operations.*/
+class UUID {
+  /**
+   * @function -random alphabet generator function, used to generate random alphabets for uuids.
+   * @returns - returns a random alphabetic character from 'a'-'z'.
+   */
+  randomAlphabet() {
+    /** @type {Array<string>}*/
+    const consonants = [
+      "b",
+      "c",
+      "d",
+      "f",
+      "g",
+      "h",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z",
+    ];
+
+    /** @type {number}*/
+    const randomIndex = Math.floor(Math.random() * consonants.length);
+
+    return consonants[randomIndex];
+  }
+
+  /**
+   * @function- generate alphanumeric UUID operation
+   * @returns - alphanumeric UUID string*/
+  alphanumeric() {
+    /** @type {string}*/
+    let array = "";
+
+    for (let index = 0; index < 4; index++) {
+      /** @type {string}*/
+      const uuid =
+        this.randomAlphabet().toLocaleUpperCase() +
+        this.randomAlphabet() +
+        Math.floor(Math.random() * 10);
+      array += uuid;
+    }
+
+    return array;
+  }
+}
+
+module.exports = UUID;
---
+++ index.js
@@ -0,0 +1,5 @@
+const DataGenerator = require("./class/dataGenerate");
+
+const dataGenerator = new DataGenerator();
+
+console.log(dataGenerator.generateRandomEmail(8));
