d9d3e977f167319f69905519e612b8c21e3d8e1b
bhandekunal16 <bhandekunal16@gmail.com>
2024-02-05 15:55:16 +0530

    update

--- class/OtpGenerator.js
+++ class/OtpGenerator.js
@@ -6,13 +6,11 @@ class OtpGenerator {
    * @returns -four digit otp
    */
   new() {
-    /** @type {number}*/
     const value = Math.floor(Math.random() * 10000);
     return value.toString().length == 3 ? parseInt(`${value}0`) : value;
   }
 
   hexNew() {
-    /** @type {number}*/
     const value = Math.floor(Math.random() * 1000000);
     return value.toString().length == 3 ? parseInt(`${value}0`) : value;
   }
