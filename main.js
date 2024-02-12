8475e527dd2d6187dc36d4a987608dc133f7e29b
bhandekunal16 <bhandekunal16@gmail.com>
2024-02-12 11:07:52 +0530

    m

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const bodyParser = require("body-parser");
 const Color = require("./interface/color");
 const Logger = require("./interface/Logger");
 const Node = require("./module/index");
-const FIle = require("./data/File");
 require("dotenv").config();
 
 const app = express();
@@ -24,11 +23,6 @@ let imports = routes.map((elements) => elements.name);
 app.listen(port, () => {
   logger.log("***************");
   logger.new(imports);
-  logger.size([
-    { name: Logger.name, path: "./interface/Logger.js" },
-    { name: Color.name, path: "./interface/color.js" },
-    { name: Node.name, path: "./module/index.js" },
-  ]);
   logger.log(`Node app is successfully created on http://localhost: ${port}.`);
   logger.log("***************");
   node.getUserInput();
