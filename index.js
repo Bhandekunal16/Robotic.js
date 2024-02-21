baefc2bccadf1e99031d6e18be921781bb7c6dab
bhandekunal16 <bhandekunal16@gmail.com>
2024-02-21 14:33:32 +0530

    The diff replaces the contents of the file `index.js` with an empty file.

--- index.js
+++
@@ -1,3 +0,0 @@
-const File = require("./data/File");
-
-console.log(new File("./package.json").size());
---
+++ interface/maintain.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const path = require("path");
+const Logger = require("../interface/Logger");
+const logger = new Logger();
+
+class Maintain {
+  log(value) {
+    try {
+      const newValue = `${new Date().toLocaleString()} - [LOG] ${value}`;
+      const folderName = "../../../logs";
+      const fileName = `${new Date().toUTCString()}.log`;
+
+      const folderPath = path.join(__dirname, `${folderName}/${fileName}`);
+
+      const filePath = path.join(folderPath, fileName);
+
+      !fs.existsSync(folderPath)
+        ? fs.mkdirSync(folderPath, { recursive: true })
+        : logger.log("folder already present.");
+
+      fs.writeFile(filePath, value, (err) => {
+        err
+          ? logger.error("Error creating file:", err)
+          : logger.log(`File "${fileName}" created successfully.`);
+      });
+    } catch (error) {
+      return error;
+    }
+  }
+}
+
+module.exports = Maintain;
