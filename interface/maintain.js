a0df37cfa1ea916b76a136a12e19bd75d397246e
bhandekunal16 <bhandekunal16@gmail.com>
2024-02-21 17:44:01 +0530

    update

--- interface/maintain.js
+++ interface/maintain.js
@@ -6,27 +6,28 @@ const logger = new Logger();
 class Maintain {
   log(value) {
     try {
-      const newValue = `${date.getFullYear()}-${(date.getMonth() + 1)
+      const date = new Date();
+      const formattedDate = `${date.getFullYear()}-${(date.getMonth() + 1)
         .toString()
-        .padStart(2, "0")}-${date
-        .getDate()
-        .toString()
-        .padStart(2, "0")} - [LOG] ${value}`;
+        .padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")}`;
+
+      const newValue = `${new Date().toLocaleString()} - [LOG] ${value}\n`;
       const folderName = "../../../logs";
-      const fileName = `${new Date().toUTCString()}.log`;
+      const fileName = `${formattedDate}.log`;
 
       const folderPath = path.join(__dirname, `${folderName}/${fileName}`);
-
       const filePath = path.join(folderPath, fileName);
 
       !fs.existsSync(folderPath)
         ? fs.mkdirSync(folderPath, { recursive: true })
         : logger.log("folder already present.");
 
-      fs.writeFile(filePath, newValue, (err) => {
+      fs.writeFile(filePath, newValue, { flag: "a" }, (err) => {
         err
           ? logger.error("Error creating file:", err)
-          : logger.log(`File "${fileName}" created successfully.`);
+          : logger.log(
+              `Message "${newValue.trim()}" appended to "${fileName}" successfully.`
+            );
       });
     } catch (error) {
       return error;
