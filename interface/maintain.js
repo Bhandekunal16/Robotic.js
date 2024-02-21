8933f7e861958c1eaec7de619dd7eeeb0f80bea9
bhandekunal16 <bhandekunal16@gmail.com>
2024-02-21 16:22:22 +0530

    update

--- interface/maintain.js
+++ interface/maintain.js
@@ -6,7 +6,12 @@ const logger = new Logger();
 class Maintain {
   log(value) {
     try {
-      const newValue = `${new Date().toLocaleString()} - [LOG] ${value}`;
+      const newValue = `${date.getFullYear()}-${(date.getMonth() + 1)
+        .toString()
+        .padStart(2, "0")}-${date
+        .getDate()
+        .toString()
+        .padStart(2, "0")} - [LOG] ${value}`;
       const folderName = "../../../logs";
       const fileName = `${new Date().toUTCString()}.log`;
 
