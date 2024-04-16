abca1ad27d6f4bfda56dae3eb6cc4ef3c79e1701
bhandekunal16 <bhandekunal16@gmail.com>
2024-04-16 15:40:18 +0530

    Delete various files and classes related to file and module creation operations, code generation, and utility classes.

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var response = require("./src/class/response");
-var Res = new response();
-function main() {
-    return Res.created("new");
-}
-console.log(main());
