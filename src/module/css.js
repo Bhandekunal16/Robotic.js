3882c4a4564e7339681045d4d129cc0e8c29d51f
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-29 17:39:30 +0530

    "Removed unnecessary blank lines in Css class"

--- src/module/css.js
+++ src/module/css.js
@@ -20,11 +20,9 @@ class Css {
     try {
       const folderPath = path.join(__dirname, `${folderName}/${trimmed}`);
       const filePath = path.join(folderPath, fileName);
-
       if (!fs.existsSync(folderPath))
         fs.mkdirSync(folderPath, { recursive: true });
       else new Logger().log("Folder already present.");
-
       fs.writeFile(filePath, this.fileContent, (err) => {
         err
           ? new Logger().error("Error creating file:", err)
