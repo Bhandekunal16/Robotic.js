a244c7f18b600fab1182b8819e7ae7c279f3796a
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-29 17:39:01 +0530

    "Reformat require statements in css.js"

--- src/module/css.js
+++ src/module/css.js
@@ -1,6 +1,8 @@
-const fs = require("fs");
-const path = require("path");
-const Logger = require("../interface/Logger");
+const [fs, path, Logger] = [
+  require("fs"),
+  require("path"),
+  require("../interface/Logger"),
+];
 
 class Css {
   fileContent = `body, h1, p { margin: 0; padding: 0;}
