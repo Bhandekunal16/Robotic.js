f218852da8113e5e1e2bda5de01e22a8ef16f4cf
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-29 17:43:15 +0530

    "Removed logger instance variable, replaced with new Logger() instances"

--- src/module/go.js
+++ src/module/go.js
@@ -1,19 +1,12 @@
 const fs = require("fs");
 const path = require("path");
 const Logger = require("../interface/Logger");
-const logger = new Logger();
 
-/** Class representing Go file operations.*/
 class Go {
   fileContent = ` package main
                   import "fmt"
       func functionName(parameter1 type1, parameter2 type2) returnType { return returnValue }
       func main() { result := functionName(value1, value2)}`;
-
-  /**Creates a Go file with the specified name.
-   * @function create a dummy go file
-   * @param {string} name - The name of the file.
-   * @returns {string|undefined} The name of the created file or undefined if there's an error.*/
   create(name) {
     const fileName = `${name}`;
     const folderName = "../../../src";
@@ -25,16 +18,16 @@ class Go {
       if (!fs.existsSync(folderPath)) {
         fs.mkdirSync(folderPath, { recursive: true });
       } else {
-        logger.log("Folder already present.");
+        new Logger().log("Folder already present.");
       }
 
       fs.writeFile(filePath, this.fileContent, (err) => {
         err
-          ? logger.error("Error creating file:", err)
-          : logger.log(`File "${fileName}" created successfully.`);
+          ? new Logger().error("Error creating file:", err)
+          : new Logger().log(`File "${fileName}" created successfully.`);
       });
     } catch (error) {
-      logger.error(error);
+      new Logger().error(error);
       return error;
     }
   }
