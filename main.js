97aa105bc007bb11e18d4fd9d05a11ee093cae9c
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-29 17:13:43 +0530

    "Reformat require statements to use array destructuring"

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const Color = require("./src/interface/color");
-const Logger = require("./src/interface/Logger");
-const Node = require("./src/module/index");
-const Response = require("./src/class/response");
+const [express, bodyParser, Color, Logger, Node, Response] = [
+  require("express"),
+  require("body-parser"),
+  require("./src/interface/color"),
+  require("./src/interface/Logger"),
+  require("./src/module/index"),
+  require("./src/class/response"),
+];
 require("dotenv").config();
 
 const app = express();
