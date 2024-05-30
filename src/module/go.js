4e10e5cb7f25915b533529b2c5edb5305c1153bb
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-30 13:26:18 +0530

    "Refactor error handling in Go class: remove explicit logging and return Error object instead"

--- src/module/go.js
+++ src/module/go.js
@@ -23,8 +23,7 @@ class Go {
           : new Logger().log(`File "${fileName}" created successfully.`);
       });
     } catch (error) {
-      new Logger().error(error);
-      return error;
+      return new Error(error);
     }
   }
 }
