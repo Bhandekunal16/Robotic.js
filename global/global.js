e99c265e5c484be641955aa0ece886fe3ebde6cd
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-30 13:24:48 +0530

    "Moved HTML template from HTML class to Global class and updated HTML class to use Global's html property."

--- global/global.js
+++ global/global.js
@@ -15,6 +15,19 @@ class Global {
                   import "fmt"
       func functionName(parameter1 type1, parameter2 type2) returnType { return returnValue }
       func main() { result := functionName(value1, value2)}`;
+    this.html = `<!DOCTYPE html> <html lang="en">
+      <head> <title>Your Page Title</title> <link rel="stylesheet" href="styles.css"> </head>
+      <body>
+        <header> <h1>Your Website</h1> <p>Welcome to your website!</p> </header>
+        <nav> <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#contact">Contact</a></li> </ul> </nav>
+        <main>  <section id="home"> <h2>Home</h2> <p>This is the home section of your website.</p> </section>
+                <section id="about"> <h2>About Us</h2> <p>Learn more about our company and values.</p> </section>
+                <section id="services"> <h2>Our Services</h2> <p>Discover the services we offer to our clients.</p> </section> </main>
+        <footer> <p>&copy; 2024 Your Website. All rights reserved.</p> </footer> </body></html>`;
   }
 }
 
--- src/module/html.js
+++ src/module/html.js
@@ -1,22 +1,10 @@
-const [fs, path, Logger] = [
+const [fs, path, Logger, Global] = [
   require("fs"),
   require("path"),
   require("../interface/Logger"),
+  require("../../global/global"),
 ];
 class HTML {
-  fileContent = `<!DOCTYPE html> <html lang="en">
-  <head> <title>Your Page Title</title> <link rel="stylesheet" href="styles.css"> </head>
-  <body>
-    <header> <h1>Your Website</h1> <p>Welcome to your website!</p> </header>
-    <nav> <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#contact">Contact</a></li> </ul> </nav>
-    <main>  <section id="home"> <h2>Home</h2> <p>This is the home section of your website.</p> </section>
-            <section id="about"> <h2>About Us</h2> <p>Learn more about our company and values.</p> </section>
-            <section id="services"> <h2>Our Services</h2> <p>Discover the services we offer to our clients.</p> </section> </main>
-    <footer> <p>&copy; 2024 Your Website. All rights reserved.</p> </footer> </body></html>`;
   create(name) {
     const [fileName, folderName, trimmed] = [
       `${name}`,
@@ -29,7 +17,7 @@ class HTML {
       !fs.existsSync(folderPath)
         ? fs.mkdirSync(folderPath, { recursive: true })
         : new Logger().log("Folder already present.");
-      fs.writeFile(filePath, this.fileContent, (err) => {
+      fs.writeFile(filePath, new Global().html, (err) => {
         err
           ? new Logger().error("Error creating file:", err)
           : new Logger().log(`File "${fileName}" created successfully.`);
