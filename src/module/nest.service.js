b5edbdab1670f02dc316037cf1f65d0d667b0a8c
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-30 10:18:07 +0530

    "Refactor import statements in NestService.js"

--- src/module/nest.service.js
+++ src/module/nest.service.js
@@ -1,6 +1,8 @@
-const fs = require("fs");
-const path = require("path");
-const Logger = require("../interface/Logger");
+const [fs, path, Logger] = [
+  require("fs"),
+  require("path"),
+  require("../interface/Logger"),
+];
 class NestService {
   create(name) {
     const fileContent = ` import { Injectable } from '@nestjs/common';
