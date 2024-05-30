2b2a16a0376925991f77f9a802c79470536a2192
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-30 16:24:25 +0530

    "Updated NestController to import Type and use it for path and log message"

--- src/module/nest.controller.js
+++ src/module/nest.controller.js
@@ -1,7 +1,8 @@
-const [fs, path, Logger] = [
+const [fs, path, Logger, Type] = [
   require("fs"),
   require("path"),
   require("../interface/Logger"),
+  require("../../global/global"),
 ];
 class NestController {
   create(name) {
@@ -16,14 +17,14 @@ class NestController {
                           findAll(): string[] { return this.${name}Service.findAll();}}`;
       const [fileName, folderName] = [
         `${name + ".controller.ts"}`,
-        "../../../src",
+        new Type().path,
       ];
       try {
         const folderPath = path.join(__dirname, `${folderName}/${name}`);
         const filePath = path.join(folderPath, fileName);
         !fs.existsSync(folderPath)
           ? fs.mkdirSync(folderPath, { recursive: true })
-          : new Logger().log("folder already present.");
+          : new Logger().log(new Type().alreadyPresent);
         fs.writeFile(filePath, fileContent, (err) => {
           err
             ? new Logger().error("Error creating file:" + err)
