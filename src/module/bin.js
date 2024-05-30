c79ccd6f21ea84515519f8075ca10e86b4f4a47f
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-30 09:27:05 +0530

    "Refactor folder creation logic in Binary class to use conditional expression instead of if-else statement."

--- src/module/bin.js
+++ src/module/bin.js
@@ -15,9 +15,9 @@ class Binary {
     try {
       const folderPath = path.join(__dirname, `${folderName}/${trimmed}`);
       const filePath = path.join(folderPath, fileName);
-      if (!fs.existsSync(folderPath))
-        fs.mkdirSync(folderPath, { recursive: true });
-      else new Logger().log("Folder already present.");
+      !fs.existsSync(folderPath)
+        ? fs.mkdirSync(folderPath, { recursive: true })
+        : new Logger().log("Folder already present.");
       fs.writeFile(filePath, this.fileContent, (err) => {
         if (err) {
           new Logger().error("Error creating file:", err);
