71e4feefcd2c2e092b9a4a3a380fe9efa199c794
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-30 16:05:51 +0530

    "Replace Global with Type in HTML class"

--- src/module/html.js
+++ src/module/html.js
@@ -1,4 +1,4 @@
-const [fs, path, Logger, Global] = [
+const [fs, path, Logger, Type] = [
   require("fs"),
   require("path"),
   require("../interface/Logger"),
@@ -8,7 +8,7 @@ class HTML {
   create(name) {
     const [fileName, folderName, trimmed] = [
       `${name}`,
-      new Global().path,
+      new Type().path,
       name.split(".")[0],
     ];
     try {
@@ -16,8 +16,8 @@ class HTML {
       const filePath = path.join(folderPath, fileName);
       !fs.existsSync(folderPath)
         ? fs.mkdirSync(folderPath, { recursive: true })
-        : new Logger().log(new Global().alreadyPresent);
-      fs.writeFile(filePath, new Global().html, (err) => {
+        : new Logger().log(new Type().alreadyPresent);
+      fs.writeFile(filePath, new Type().html, (err) => {
         err
           ? new Error(error)
           : new Logger().log(`File "${fileName}" created successfully.`);
