45350091be9689b622858f3d80b0767d13c78d62
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-30 16:07:26 +0530

    "Replace Global with Type in JavaScript class"

--- src/module/javaScript.js
+++ src/module/javaScript.js
@@ -1,4 +1,4 @@
-const [fs, path, Logger, Global] = [
+const [fs, path, Logger, Type ] = [
   require("fs"),
   require("path"),
   require("../interface/Logger"),
@@ -8,7 +8,7 @@ class JavaScript {
   create(name) {
     const [fileName, folderName, trimmed] = [
       `${name}`,
-      new Global().path,
+      new Type().path,
       name.split(".")[0],
     ];
     try {
@@ -16,8 +16,8 @@ class JavaScript {
       const filePath = path.join(folderPath, fileName);
       !fs.existsSync(folderPath)
         ? fs.mkdirSync(folderPath, { recursive: true })
-        : new Logger().log(new Global().alreadyPresent);
-      fs.writeFile(filePath, new Global().javascript, (err) => {
+        : new Logger().log(new Type().alreadyPresent);
+      fs.writeFile(filePath, new Type().javascript, (err) => {
         err
           ? new Error(err)
           : new Logger().log(`File "${fileName}" created successfully.`);
