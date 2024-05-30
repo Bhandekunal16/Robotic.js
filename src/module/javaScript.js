37b6c27a1c426f9346151ade20d8137615363d55
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-30 09:20:47 +0530

    "Removed unnecessary lines and whitespace in JavaScript class"

--- src/module/javaScript.js
+++ src/module/javaScript.js
@@ -11,13 +11,10 @@ class JavaScript {
     const trimmed = name.split(".")[0];
     try {
       const folderPath = path.join(__dirname, `${folderName}/${trimmed}`);
-
       const filePath = path.join(folderPath, fileName);
-
       !fs.existsSync(folderPath)
         ? fs.mkdirSync(folderPath, { recursive: true })
         : new Logger().log("folder already present.");
-
       fs.writeFile(filePath, this.fileContent, (err) => {
         err
           ? new Logger().error("Error creating file:", err)
