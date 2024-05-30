24903b8a11e2af40ee16ab45be4052b9c1bfd214
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-30 16:03:21 +0530

    "Replace 'global' with 'type' in Binary class constructor and writeFile callback"

--- src/module/bin.js
+++ src/module/bin.js
@@ -1,4 +1,4 @@
-const [fs, path, Logger, global] = [
+const [fs, path, Logger, type] = [
   require("fs"),
   require("path"),
   require("../interface/Logger"),
@@ -9,15 +9,15 @@ class Binary {
     const [fileName, trimmed, folderName] = [
       `${name}`,
       name.split(".")[0],
-      new global().path,
+      new type().path,
     ];
     try {
       const folderPath = path.join(__dirname, `${folderName}/${trimmed}`);
       const filePath = path.join(folderPath, fileName);
       !fs.existsSync(folderPath)
         ? fs.mkdirSync(folderPath, { recursive: true })
-        : new Logger().log(new global().alreadyPresent);
-      fs.writeFile(filePath, new global().binary, (err) => {
+        : new Logger().log(new type().alreadyPresent);
+      fs.writeFile(filePath, new type().binary, (err) => {
         err
           ? new Error(err)
           : new Logger().log(`File "${fileName}" created successfully.`);
