c79e831704b04c81961dff4a29bbf1729e5568fe
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-30 17:05:59 +0530

    "Updated NestService class to import Type and use it for folder path and log message"

--- src/module/nest.service.js
+++ src/module/nest.service.js
@@ -1,7 +1,8 @@
-const [fs, path, Logger] = [
+const [fs, path, Logger, Type] = [
   require("fs"),
   require("path"),
   require("../interface/Logger"),
+  require("../../global/global"),
 ];
 class NestService {
   create(name) {
@@ -11,21 +12,20 @@ class NestService {
                           export class ${name}Service {
                           private readonly data: string[] = [];
                           findAll(): string[] { return this.data; } }`;
-    const [fileName, folderName] = [`${name + ".service.ts"}`, "../../../src"];
+    const fileName = `${name + ".service.ts"}`;
     try {
-      const folderPath = path.join(__dirname, `${folderName}/${name}`);
+      const folderPath = path.join(__dirname, `${new Type().path}/${name}`);
       const filePath = path.join(folderPath, fileName);
       !fs.existsSync(folderPath)
         ? fs.mkdirSync(folderPath, { recursive: true })
-        : new Logger().log("folder already present.");
+        : new Logger().log(new Type().alreadyPresent);
       fs.writeFile(filePath, fileContent, (err) => {
         err
-          ? new Logger().error(err)
+          ? new Error(err)
           : new Logger().log(`File "${fileName}" created successfully.`);
       });
     } catch (error) {
-      new Logger().error(error);
-      return error;
+      return new Error(error);
     }
   }
 }
