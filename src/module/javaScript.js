1534f15587e0466d90030f1e590a287fb1314504
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-30 09:20:09 +0530

    "Removed class comment and method comment from JavaScript class"

--- src/module/javaScript.js
+++ src/module/javaScript.js
@@ -3,14 +3,8 @@ const path = require("path");
 const Logger = require("../interface/Logger");
 const logger = new Logger();
 
-/**Class representing Java file operations.*/
 class JavaScript {
   fileContent = `function main(){ try{ }catch(error){ return {res: error , status: false, msg: 'error'}}}`;
-  
-  /**Creates a javascript file with the specified name.
-   * @function  create a dummy javascript file.
-   * @param {string} name - The name of the file.
-   * @returns {string|undefined} The name of the created file or undefined if there's an error.*/
   create(name) {
     const fileName = `${name}`;
     const folderName = "../../../src";
