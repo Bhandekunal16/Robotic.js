3a9a05981278c58200fb90fc4d1a98c2a7a5d582
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-30 16:01:22 +0530

    "Added type annotations and initialized several string properties in the Global class."

--- global/global.js
+++ global/global.js
@@ -1,8 +1,12 @@
 class Global {
   constructor() {
+    /** @type string */ 
     this.binary = "1000001";
+    /** @type string */ 
     this.path = "../../../src";
+    /** @type string */ 
     this.alreadyPresent = "Folder already present.";
+    /** @type string */ 
     this.css = `body, h1, p { margin: 0; padding: 0;}
     body { font-family: 'Arial', sans-serif; background-color: #f0f0f0; color: #333; }
     .container { width: 80%; margin: 0 auto; }
@@ -11,10 +15,12 @@ class Global {
     nav a { color: #ecf0f1; text-decoration: none; margin-right: 1em; }
     main { padding: 1em; }
     footer { background-color: #2c3e50; color: #ecf0f1; padding: 1em; text-align: center;}`;
+    /** @type string */ 
     this.go = ` package main
                   import "fmt"
       func functionName(parameter1 type1, parameter2 type2) returnType { return returnValue }
       func main() { result := functionName(value1, value2)}`;
+    /** @type string */ 
     this.html = `<!DOCTYPE html> <html lang="en">
       <head> <title>Your Page Title</title> <link rel="stylesheet" href="styles.css"> </head>
       <body>
@@ -28,11 +34,14 @@ class Global {
                 <section id="about"> <h2>About Us</h2> <p>Learn more about our company and values.</p> </section>
                 <section id="services"> <h2>Our Services</h2> <p>Discover the services we offer to our clients.</p> </section> </main>
         <footer> <p>&copy; 2024 Your Website. All rights reserved.</p> </footer> </body></html>`;
+    /** @type string */ 
     this.start = 'Enter something (type "exit" to stop): ';
+    /** @type string */ 
     this.java = `public class MyClass {
       public static ReturnType myFunction(ParameterType1 parameter1, ParameterType2 parameter2) { return returnValue;}
       public static void main(String[] args) {
           ReturnType result = myFunction(value1, value2);}}`;
+    /** @type string */ 
     this.javascript = `function main(){ try{ }catch(error){ return {res: error , status: false, msg: 'error'}}}`;
   }
 }
