06cca6316326897573ff712a72112e0273f540ae
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-30 09:29:32 +0530

    "Replace static logger with new Logger() instance"

--- src/module/map.js
+++ src/module/map.js
@@ -19,15 +19,15 @@ class Map {
 
       !fs.existsSync(folderPath)
         ? fs.mkdirSync(folderPath, { recursive: true })
-        : logger.log("folder already present.");
+        : new Logger().log("folder already present.");
 
       fs.writeFile(filePath, fileContent, (err) => {
         err
-          ? logger.error("Error creating file:", err)
-          : logger.log(`File "${fileName}" created successfully.`);
+          ? new Logger().error("Error creating file:", err)
+          : new Logger().log(`File "${fileName}" created successfully.`);
       });
     } catch (error) {
-      logger.error(error);
+      new Logger().error(error);
       return error;
     }
   }
