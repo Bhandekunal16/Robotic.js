f001bced224b353ebf07e0b5dced14f9ec2778a6
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-05 09:44:37 +0530

    "Refactor JSON class: simplify create method and remove dependencies on fs, path, and Logger"

--- src/module/package.js
+++ src/module/package.js
@@ -1,26 +1,10 @@
-const [fs, path, Logger, Type] = [
-  require("fs"),
-  require("path"),
-  require("../interface/Logger"),
+const [create, type] = [
+  require("../data/create"),
   require("../../global/global"),
 ];
 class JSON {
   create(name) {
-    const [fileName, trimmed] = [name.toString(), name.split(".")[0]];
-    try {
-      const folderPath = path.join(__dirname, `${new Type().path}/${trimmed}`);
-      const filePath = path.join(folderPath, fileName);
-      !fs.existsSync(folderPath)
-        ? fs.mkdirSync(folderPath, { recursive: true })
-        : new Logger().log(new Type().alreadyPresent);
-      fs.writeFile(filePath, new Type().package, (err) => {
-        err
-          ? new Error(err)
-          : new Logger().log(`File "${fileName}" created successfully.`);
-      });
-    } catch (error) {
-      return new Error(error);
-    }
+    return new create().create(name, new type().package);
   }
 }
 module.exports = JSON;
