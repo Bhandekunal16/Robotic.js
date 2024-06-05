3cb814e834888165745f546c79255148af429804
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-05 09:35:10 +0530

    "Refactor NestDtoCreate class to use create function from ../data/create instead of fs and path modules"

--- src/module/Nest-create-dto.js
+++ src/module/Nest-create-dto.js
@@ -1,32 +1,11 @@
-const [fs, path, Logger, Type] = [
-  require("fs"),
-  require("path"),
-  require("../interface/Logger"),
-  require("../../global/global"),
-];
+const create = require("../data/create");
 class NestDtoCreate {
   create(name) {
     const [fileContent, fileName] = [
       `export class create${name}Dto { }`,
       `create.${name}.dto.ts`,
     ];
-    try {
-      const folderPath = path.join(
-        __dirname,
-        `${new Type().path}/${name}/dto/`
-      );
-      const filePath = path.join(folderPath, fileName);
-      !fs.existsSync(folderPath)
-        ? fs.mkdirSync(folderPath, { recursive: true })
-        : new Logger().log(new Type().alreadyPresent);
-      fs.writeFile(filePath, fileContent, (err) => {
-        err
-          ? new Error(err)
-          : new Logger().log(`File "${fileName}" created successfully.`);
-      });
-    } catch (error) {
-      return new Error(error);
-    }
+    return new create().create(fileName, fileContent);
   }
 }
 module.exports = NestDtoCreate;
