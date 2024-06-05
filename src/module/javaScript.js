d3cc6488c60cf0033259afb51de6c4e3d71a0fbb
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-05 09:27:17 +0530

    "Refactor JavaScript class to use create and type modules, removing fs, path, and Logger dependencies"

--- src/module/javaScript.js
+++ src/module/javaScript.js
@@ -1,26 +1,10 @@
-const [fs, path, Logger, Type] = [
-  require("fs"),
-  require("path"),
-  require("../interface/Logger"),
+const [create, type] = [
+  require("../data/create"),
   require("../../global/global"),
 ];
 class JavaScript {
   create(name) {
-    const [fileName, trimmed] = [name.toString(), name.split(".")[0]];
-    try {
-      const folderPath = path.join(__dirname, `${new Type().path}/${trimmed}`);
-      const filePath = path.join(folderPath, fileName);
-      !fs.existsSync(folderPath)
-        ? fs.mkdirSync(folderPath, { recursive: true })
-        : new Logger().log(new Type().alreadyPresent);
-      fs.writeFile(filePath, new Type().javascript, (err) => {
-        err
-          ? new Error(err)
-          : new Logger().log(`File "${fileName}" created successfully.`);
-      });
-    } catch (error) {
-      return new Error(error);
-    }
+    return new create().create(name, new type().javascript);
   }
 }
 module.exports = JavaScript;
