c760d30b1a5b14ba2d3b19427b0ef2a848f19520
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-05 09:40:49 +0530

    "Refactor NestModule class to remove dependencies and simplify create method"

--- src/module/nest.module.js
+++ src/module/nest.module.js
@@ -1,9 +1,4 @@
-const [fs, path, Logger, Type] = [
-  require("fs"),
-  require("path"),
-  require("../interface/Logger"),
-  require("../../global/global"),
-];
+const create = require("../data/create");
 class NestModule {
   create(name) {
     const fileContent = ` import { Module } from '@nestjs/common';
@@ -12,20 +7,7 @@ class NestModule {
                           @Module({ controllers: [${name}Controller], providers: [${name}Service], }) 
                           export class ${name}Module {}`;
     const fileName = `${name + ".module.ts"}`;
-    try {
-      const folderPath = path.join(__dirname, `${new Type().path}/${name}`);
-      const filePath = path.join(folderPath, fileName);
-      !fs.existsSync(folderPath)
-        ? fs.mkdirSync(folderPath, { recursive: true })
-        : new Logger().log(new Type().alreadyPresent);
-      fs.writeFile(filePath, fileContent, (err) => {
-        err
-          ? new Error(err)
-          : new Logger().log(`File "${fileName}" created successfully.`);
-      });
-    } catch (error) {
-      return new Error(error);
-    }
+    return new create().create(fileName, fileContent);
   }
 }
 module.exports = NestModule;
