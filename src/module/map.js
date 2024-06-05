641ead1d9e48d4b5d7a8665a4cbd1c23260e914e
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-05 09:32:41 +0530

    "Update path property in Type class from '../../../src' to '../src'"

--- src/module/map.js
+++ src/module/map.js
@@ -1,9 +1,5 @@
-const [fs, path, Logger, Type] = [
-  require("fs"),
-  require("path"),
-  require("../interface/Logger"),
-  require("../../global/global"),
-];
+const create = require("../data/create");
+
 class Map {
   create(name) {
     const trimmed = name.split(".")[0];
@@ -11,20 +7,7 @@ class Map {
       `${trimmed}.js`,
       `class ${trimmed} {} module.exports = ${trimmed};`,
     ];
-    try {
-      const folderPath = path.join(__dirname, `${new Type().path}`);
-      const filePath = path.join(folderPath, fileName);
-      !fs.existsSync(folderPath)
-        ? fs.mkdirSync(folderPath, { recursive: true })
-        : new Logger().log(new Type().alreadyPresent);
-      fs.writeFile(filePath, fileContent, (err) => {
-        err
-          ? new Error(err)
-          : new Logger().log(`File "${fileName}" created successfully.`);
-      });
-    } catch (error) {
-      return new Error(error);
-    }
+    return new create().create(fileName, fileContent);
   }
 }
 module.exports = Map;
