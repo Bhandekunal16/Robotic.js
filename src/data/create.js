30b3ea1d1cdcbfdf6c65a351ab4d388cefbbb071
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-05 09:12:02 +0530

    "Refactor Binary class: simplify create method and remove dependencies on fs, path, and Logger"

---
+++ src/data/create.js
@@ -0,0 +1,28 @@
+const [fs, path, Logger, type] = [
+  require("fs"),
+  require("path"),
+  require("../interface/Logger"),
+  require("../../global/global"),
+];
+
+class create {
+  create(name, content) {
+    const [fileName, trimmed] = [name.toString(), name.split(".")[0]];
+    try {
+      const folderPath = path.join(__dirname, `${new type().path}/${trimmed}`);
+      const filePath = path.join(folderPath, fileName);
+      !fs.existsSync(folderPath)
+        ? fs.mkdirSync(folderPath, { recursive: true })
+        : new Logger().log(new type().alreadyPresent);
+      fs.writeFile(filePath, content, (err) => {
+        err
+          ? new Error(err)
+          : new Logger().log(`File "${fileName}" created successfully.`);
+      });
+    } catch (error) {
+      return new Error(error);
+    }
+  }
+}
+
+module.exports = create;
--- src/module/bin.js
+++ src/module/bin.js
@@ -1,26 +1,10 @@
-const [fs, path, Logger, type] = [
-  require("fs"),
-  require("path"),
-  require("../interface/Logger"),
+const [create, type] = [
+  require("../data/create"),
   require("../../global/global"),
 ];
 class Binary {
   create(name) {
-    const [fileName, trimmed] = [name.toString(), name.split(".")[0]];
-    try {
-      const folderPath = path.join(__dirname, `${new type().path}/${trimmed}`);
-      const filePath = path.join(folderPath, fileName);
-      !fs.existsSync(folderPath)
-        ? fs.mkdirSync(folderPath, { recursive: true })
-        : new Logger().log(new type().alreadyPresent);
-      fs.writeFile(filePath, new type().binary, (err) => {
-        err
-          ? new Error(err)
-          : new Logger().log(`File "${fileName}" created successfully.`);
-      });
-    } catch (error) {
-      return new Error(error);
-    }
+    return new create().create(name, new type().binary);
   }
 }
 module.exports = Binary;
