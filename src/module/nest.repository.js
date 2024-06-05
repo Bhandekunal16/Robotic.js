ed68f9026fe5dd4842c2cd566f5be1e183d98de5
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-05 09:41:46 +0530

    "Simplified `create` method in `NestRepository` class by extracting file creation logic into a separate `create` function."

--- src/module/nest.repository.js
+++ src/module/nest.repository.js
@@ -6,30 +6,13 @@ const [fs, path, Logger, Type] = [
 ];
 class NestRepository {
   create(name) {
-    try {
-      const fileContent = ` import { Injectable } from '@nestjs/common';
-                            import { create${name}Dto } from './dto/create.${name}.dto';
-                            @Injectable()
-                            export class ${name}Repository { private readonly data: string[] = [];
-                            findAll(): string[] { return this.data; } }`;
-      const fileName = `${name + ".repository.ts"}`;
-      try {
-        const folderPath = path.join(__dirname, `${new Type().path}/${name}`);
-        const filePath = path.join(folderPath, fileName);
-        !fs.existsSync(folderPath)
-          ? fs.mkdirSync(folderPath, { recursive: true })
-          : new Logger().log(new Type().alreadyPresent);
-        fs.writeFile(filePath, fileContent, (err) => {
-          err
-            ? new Error(err)
-            : new Logger().log(`File "${fileName}" created successfully.`);
-        });
-      } catch (error) {
-        return new Error(error);
-      }
-    } catch (error) {
-      return new Error(error);
-    }
+    const fileContent = ` import { Injectable } from '@nestjs/common';
+    import { create${name}Dto } from './dto/create.${name}.dto';
+    @Injectable()
+    export class ${name}Repository { private readonly data: string[] = [];
+    findAll(): string[] { return this.data; } }`;
+    const fileName = `${name + ".repository.ts"}`;
+    return new create().create(fileName, fileContent);
   }
 }
 module.exports = NestRepository;
