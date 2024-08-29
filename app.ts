adf0eb12eb0fbb0ebece8751fcba36e134455453
bhandekunal16 <bhandekunal16@gmail.com>
2024-08-29 10:03:03 +0530

    "Update version in package.json from 1.4.8 to 1.4.9"

---
+++ app.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const logger = require("./src/interface/Logger");
+const response = require("./src/class/response");
+const Uuid = require("./src/class/uuid");
+const OTP = require("./src/class/OtpGenerator");
+
+import uuid from "./types/class/uuid";
+import otp from "./types/class/OtpGenerator";
+import Logger from "./types/interface/Logger";
+
+export const LOGGER: Logger = new logger();
+export const Res: Response = new response();
+export const UUID: uuid = new Uuid();
+export const Otp: otp = new OTP();
