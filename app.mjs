b3c8d33a2bdcf5641b84180a700088b4982ea848
bhandekunal16 <bhandekunal16@gmail.com>
2024-09-04 17:48:29 +0530

    "Update version in package.json from 1.4.9 to 1.4.10"

---
+++ app.mjs
@@ -0,0 +1,28 @@
+import Logger from "./src/interface/Logger.js";
+import Color from "./src/interface/color.js";
+import Maintainer from "./src/interface/maintain.js";
+import Timestamp from "./src/interface/time.js";
+import DataGenerator from "./src/class/dataGenerate.js";
+import OtpGenerator from "./src/class/OtpGenerator.js";
+import HexColorGenerator from "./src/class/random-hexColor-generator.js";
+import Response from "./src/class/response.js";
+import TypeChecker from "./src/class/TypeChecker.js";
+import UUID from "./src/class/uuid.js";
+import Create from "./src/data/create.js";
+import File from "./src/data/File.js";
+import mathematic from "./src/data/mathematic.js";
+
+export const custom = new Logger();
+export const color = new Color();
+export const maintain = new Maintainer();
+export const time = new Timestamp();
+export const dataGenerate = new DataGenerator();
+export const otpGenerator = new OtpGenerator();
+export const hexColorGenerator = new HexColorGenerator();
+export const response = new Response();
+export const typeChecker = new TypeChecker();
+export const uuid = new UUID();
+export const create = new Create();
+export const file = new File();
+export const math = new mathematic();
+
