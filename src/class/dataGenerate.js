011272d9ea63b2e6d9f92e9c802ad6ec52a9c695
bhandekunal16 <bhandekunal16@gmail.com>
2024-09-24 16:41:03 +0530

    "Refactored DataGenerator class: updated imports, added private fields, and modified method implementations"

--- src/class/dataGenerate.js
+++ src/class/dataGenerate.js
@@ -1,41 +1,45 @@
-const TypeChecker = require("./TypeChecker");
-const Type = require("../../global/global");
+const [TypeChecker, Type] = [
+  require("./TypeChecker"),
+  require("../../global/global"),
+];
 
 class DataGenerator {
+  #type;
+  #checker;
+  value;
+
   constructor() {
-    this.consonants = new Type().consonants;
-    this.ovals = new Type().ovals;
-    this.emails = new Type().emails;
-    this.error = new Type().error;
+    this.#type = new Type();
+    this.#checker = new TypeChecker();
   }
 
   create(length, type) {
-    const [checkLength, checkType] = [
-      new TypeChecker().checkNumber(length),
-      new TypeChecker().checkString(type),
-    ];
-    const lengthValue = checkLength && checkType ? length : 0;
     try {
+      const [checkLength, checkType] = [
+        this.#checker.checkNumber(length),
+        this.#checker.checkString(type),
+      ];
+      const lengthValue = checkLength && checkType ? length : 0;
       let value;
       switch (true) {
         case type == "name":
-          value = this.generateRandomName(lengthValue);
+          value = this.#generateRandomName(lengthValue);
           return value;
         case type == "mobileNo":
-          value = this.generateMobileNumber(lengthValue);
+          value = this.#generateMobileNumber(lengthValue);
           return value;
         case type == "email":
-          value = this.generateRandomEmail(lengthValue);
+          value = this.#generateRandomEmail(lengthValue);
           return value;
         default:
-          break;
+          throw new Error(`Unsupported type: ${type}`);
       }
     } catch (error) {
       return new Error(error);
     }
   }
 
-  generateMobileNumber(length) {
+  #generateMobileNumber(length) {
     try {
       const array = [];
       for (let index = 0; index < length; index++) {
@@ -44,6 +48,7 @@ class DataGenerator {
           element += Math.floor(Math.random() * 10);
         }
         name.push(element);
+        console.log(name);
         array.push(...name);
       }
       return array.length > 0 ? array : new Error(this.error);
@@ -52,18 +57,17 @@ class DataGenerator {
     }
   }
 
-  generateRandomName(length) {
+  #generateRandomName(length) {
     try {
       const array = [];
       for (let index = 0; index < length; index++) {
-        let [name, element] = [[], ""];
+        let element = "";
         for (let j = 1; j < 4; j++) {
           element +=
-            this.consonants[Math.floor(Math.random() * 20)] +
-            this.ovals[Math.floor(Math.random() * 5)];
+            this.#type.consonants[Math.floor(Math.random() * 20)] +
+            this.#type.ovals[Math.floor(Math.random() * 5)];
         }
-        name.push(element);
-        array.push(...name);
+        array.push(element);
       }
       return array.length > 0 ? array : new Error(this.error);
     } catch (error) {
@@ -71,15 +75,15 @@ class DataGenerator {
     }
   }
 
-  generateRandomEmail(length) {
+  #generateRandomEmail(length) {
     try {
       let array = [];
       for (let index = 0; index < length; index++) {
         let [name, element] = [[], ""];
         for (let j = 1; j < 7; j++) {
-          element += this.consonants[Math.floor(Math.random() * 20)];
+          element += this.#type.consonants[Math.floor(Math.random() * 20)];
         }
-        name.push(element + this.emails[Math.floor(Math.random() * 4)]);
+        name.push(element + this.#type.emails[Math.floor(Math.random() * 4)]);
         array.push(...name);
       }
       return array.length > 0 ? array : new Error(this.error);
@@ -89,13 +93,14 @@ class DataGenerator {
   }
 
   captcha() {
-    let element = "";
+    this.value = "";
     for (let j = 1; j < 5; j++) {
-      element +=
-        this.consonants[Math.floor(Math.random() * 10)].toLocaleUpperCase() +
-        this.ovals[Math.floor(Math.random() * 5)];
+      this.value +=
+        this.#type.consonants[
+          Math.floor(Math.random() * 10)
+        ].toLocaleUpperCase() + this.#type.ovals[Math.floor(Math.random() * 5)];
     }
-    return element;
+    return this.value;
   }
 }
 
